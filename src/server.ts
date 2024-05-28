380f797ae086f7af1d2ecd6a8eb89fd8ae6c992f
yaelili70 <yaelili70@gmail.com>
2024-05-28 13:09:21 +0300

    feat: add node project with express and ts

---
+++ src/server.ts
@@ -0,0 +1,12 @@
+import express from 'express';
+
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+  res.send('Hello, TypeScript with Express!');
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
