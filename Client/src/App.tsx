4c4044cabbd072024cae71bd9e9ca40149ece94d
yaelili70 <yaelili70@gmail.com>
2024-07-11 18:27:12 +0300

    create project with appropriate folders

---
+++ Client/src/App.tsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <div style={{ textAlign: 'center' }}>
+      <h1>Mindwell</h1>
+      <h6>Yael Buchris was here</h6>
+    </div>
+  )
+
+
+}
+
+export default App
