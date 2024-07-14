c9e6e3fc6da435b5616ce396b797c2715fd40d51
stav1236 <stavmaor1@gmail.com>
2024-07-15 00:18:12 +0300

    alias

--- Client/vite.config.ts
+++ Client/vite.config.ts
@@ -1,7 +1,28 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import dotenv from "dotenv";
+import { defineConfig } from "vite";
+import { fileURLToPath } from "url";
+import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
+dotenv.config();
+
 export default defineConfig({
   plugins: [react()],
-})
+  resolve: {
+    alias: [
+      {
+        find: "@",
+        replacement: fileURLToPath(new URL("./src", import.meta.url)),
+      },
+      {
+        find: "contexts",
+        replacement: fileURLToPath(new URL("./src/contexts", import.meta.url)),
+      },
+      {
+        find: "components",
+        replacement: fileURLToPath(
+          new URL("./src/components", import.meta.url)
+        ),
+      },
+    ],
+  },
+});
