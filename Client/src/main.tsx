b9ff58c79a4b1fc46ea327f5f62f9ffe04d16819
stav1236 <stavmaor1@gmail.com>
2024-07-15 00:18:27 +0300

    adding mui and theme

--- Client/src/main.tsx
+++ Client/src/main.tsx
@@ -1,10 +1,15 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+import App from "./App.tsx";
+import "./index.css";
+
+import { ThemeModeProvider } from "@/hooks/ThemeModeContext.tsx";
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <ThemeModeProvider>
+      <App />
+    </ThemeModeProvider>
+  </React.StrictMode>
+);
