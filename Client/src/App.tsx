f3e66581b33c8f0f5090a5ac9b6257128f5e4840
yaelili70 <yaelili70@gmail.com>
2024-08-04 22:14:14 +0300

    update design of sharing page

--- Client/src/App.tsx
+++ Client/src/App.tsx
@@ -2,6 +2,7 @@ import { ThemeProvider } from "@mui/material";
 import { useThemeMode } from "./hooks/ThemeModeContext";
 import LoginPage from "./pages/login/Login";
 import SignUpPage from "./pages/signup/Signup";
+import SharingPage from "./pages/sharing/SharingPage";
 import { History } from "./pages/History";
 import { Route, BrowserRouter, Routes } from "react-router-dom";
 
@@ -10,13 +11,14 @@ const App = () => {
 
   return (
     <ThemeProvider theme={theme}>
-       <BrowserRouter>
-          <Routes>
-            <Route path="/signup" element={<SignUpPage />} />
-            <Route path="/login" element={<LoginPage />} />
-            <Route path="/login" element={<History />} />
-          </Routes>
-       </BrowserRouter>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/signup" element={<SignUpPage />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/sharing" element={<SharingPage />} />
+          <Route path="/login" element={<History />} />
+        </Routes>
+      </BrowserRouter>
     </ThemeProvider>
   );
 };
