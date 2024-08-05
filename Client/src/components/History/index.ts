a6fc01f415ab0d63652439eba25e74a92e17276a
Stav Maor <stavmaor1@gmail.com>
2024-08-06 00:52:23 +0300

    search event

--- Client/src/components/History/index.ts
+++ Client/src/components/History/index.ts
@@ -1,3 +1,4 @@
 export * from "./Calendar/Calendar";
 export * from "./YearInput/YearInput";
 export * from "./MonthInput/MonthInput";
+export * from "./SearchInput/SearchInput";
--- Client/src/pages/History.tsx
+++ Client/src/pages/History.tsx
@@ -1,13 +1,14 @@
-import { Box } from "@mui/material";
+import { Box, Button } from "@mui/material";
 import { useMemo, useState } from "react";
 
 import { Month } from "@/types/enums/Month";
 import { CalendarDay } from "@/types/CalendarDay";
 import { getMonthCalendar } from "@/utilities/DateUtils";
-import { Calendar, YearInput, MonthInput } from "@/components/History";
-
+import { Calendar, SearchInput } from "@/components/History";
+import { useThemeMode } from "@/hooks/ThemeModeContext";
 
 export const History = () => {
+
   const currentDate = new Date();
   const currentYear: number = currentDate.getFullYear();
   const currentMonth: Month = currentDate.getMonth() + 1;
@@ -19,13 +20,13 @@ export const History = () => {
     return getMonthCalendar(selectedMonth, selectedYear).days;
   }, [selectedYear, selectedMonth]);
 
+  const { toggleDarkMode } = useThemeMode();
+
   return (
     <Box sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center' }}>
+      <Button onClick={toggleDarkMode}>dark mode</Button>
       <Box sx={{ m: 5, maxWidth: '600px' }}>
-        <Box sx={{ my: 2, width: '100%', display: 'flex', gap: 2, justifyContent: 'space-between', alignItems: 'center' }}>
-          <YearInput year={selectedYear} setYear={(year: number) => setSelectedYear(year)} />
-          <MonthInput month={selectedMonth} setMonth={(month: Month) => setSelectedMonth(month)} />
-        </Box>
+        <SearchInput />
         <Calendar
           month={selectedMonth}
           year={selectedYear}
