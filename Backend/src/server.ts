9853e3551d3b619b4c445625774cf8ac0caa2327
Stav Maor <stavmaor1@gmail.com>
2024-08-07 20:45:04 +0300

    enable cors

--- Backend/src/server.ts
+++ Backend/src/server.ts
@@ -6,11 +6,14 @@ import { errorHandler } from "./middleware/errorMiddleware";
 import bodyParser from "body-parser";
 import { buildMoodsRouter } from "./routers/moods/moodsRouter";
 import { buildEventsRouter } from "./routers/events/eventsRouter";
+import cors from 'cors';
 
 export const initServer = () => {
   const { PORT } = Configuration.getInstance();
 
   const app = express();
+  app.use(cors())
+
   app.use(bodyParser.json());
   app.use(buildSuggestionRouter());
   app.use(buildMoodsRouter());
