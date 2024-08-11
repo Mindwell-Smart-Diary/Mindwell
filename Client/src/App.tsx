0760695bf554f4d7b9b1525addc5a4308fad2589
Stav Maor <stavmaor1@gmail.com>
2024-08-11 21:09:21 +0300

    clean code

--- Client/src/App.tsx
+++ Client/src/App.tsx
@@ -1,4 +1,4 @@
-import {  ThemeProvider } from "@mui/material";
+import { ThemeProvider } from "@mui/material";
 import { useThemeMode } from "./hooks/ThemeModeContext";
 import LoginPage from "./pages/login/Login";
 import SignUpPage from "./pages/signup/Signup";
@@ -6,27 +6,23 @@ import SharingPage from "./pages/sharing/SharingPage";
 import { History } from "./pages/History";
 import { Route, BrowserRouter, Routes } from "react-router-dom";
 import HistoryOfSuggestionsPage from "./pages/suggestionHistory/suggestionHistory";
-import AppBarComponent from "./components/appbar/Appbar";
+import AppBarComponent from "./components/AppBar/Appbar";
 
 const App = () => {
   const { theme } = useThemeMode();
 
   return (
     <ThemeProvider theme={theme}>
-       <BrowserRouter>
-        <AppBarComponent darkMode={false} handleDarkModeToggle={function (): void {
-          throw new Error("Function not implemented.");
-        } } handleLogout={function (): void {
-          throw new Error("Function not implemented.");
-        } }/>
-          <Routes>
-            <Route path="/signup" element={<SignUpPage />} />
-            <Route path="/login" element={<LoginPage />} />
-            <Route path="/history" element={<History />} />
-            <Route path="/historySuggestions" element={<HistoryOfSuggestionsPage />} />
-            <Route path="/sharing/:year/:month/:day" element={<SharingPage />} />
-          </Routes>
-       </BrowserRouter>
+      <BrowserRouter>
+        <AppBarComponent />
+        <Routes>
+          <Route path="/signup" element={<SignUpPage />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/history" element={<History />} />
+          <Route path="/historySuggestions" element={<HistoryOfSuggestionsPage />} />
+          <Route path="/sharing/:year/:month/:day" element={<SharingPage />} />
+        </Routes>
+      </BrowserRouter>
     </ThemeProvider>
   );
 };
