6fe1699786fb7e4fde827f08c89947220c29159b
Amit Keinan <amit.keinan.9@gmail.com>
2024-08-23 17:30:35 +0300

    fix: rewrite

--- Client/vite.config.ts
+++ Client/vite.config.ts
@@ -12,8 +12,28 @@ export default defineConfig(() => {
       proxy: {
         "/api": {
           target: process.env.VITE_API_BASE_URL ?? "http://localhost:3000",
+          rewrite: (path) => path.replace(/^\/api/, ""),
           changeOrigin: true,
           secure: false,
+          configure: (proxy, _options) => {
+            proxy.on("error", (err, _req, _res) => {
+              console.log("proxy error", err);
+            });
+            proxy.on("proxyReq", (proxyReq, req, _res) => {
+              console.log(
+                "Sending Request to the Target:",
+                req.method,
+                req.url
+              );
+            });
+            proxy.on("proxyRes", (proxyRes, req, _res) => {
+              console.log(
+                "Received Response from the Target:",
+                proxyRes.statusCode,
+                req.url
+              );
+            });
+          },
         },
       },
     },
