b7add0706f7097f6b1ffc24e94571799507b44fd
nir dancig <u8689440@bsmch.net>
2024-08-26 18:59:02 +0300

    Remove title, add tooltip and fix bug in appbar

--- Client/src/App.tsx
+++ Client/src/App.tsx
@@ -49,7 +49,7 @@ const router = createBrowserRouter([
         element: <HistoryPage />,
       },
       {
-        path: "historySuggestions",
+        path: "suggestions",
         element: <HistoryOfSuggestionsPage />,
       },
       {
